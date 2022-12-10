b43ac2dd404245f6c03b88068779c78669b35a88
satyam kale <satyamkale271@gmail.com>
2022-12-11 03:19:18 +0530

    learned primitives

--- frist-javaprogram/frist-idea-program/src/com/satyam/primitives.java
+++ frist-javaprogram/frist-idea-program/src/com/satyam/primitives.java
@@ -1,4 +1,14 @@
 package com.satyam;
 
 public class primitives {
+    public static void main(String[] args) {
+        // primitive are data type that cannot break further//
+        // note string is not a primitive //
+        int rollno = 64; // to store integer value//
+        char letter = 'r'; // stores character //
+        float mark = 98.67f;
+        double large = 45156.25448; // it is for large decimal numbers//
+        long lagreint = 25489635; // it is for large integer values//
+
+    }
 }
