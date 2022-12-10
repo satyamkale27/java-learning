bc65dee0c3ffb396541bee394abe8c7b0562ed3b
satyam kale <satyamkale271@gmail.com>
2022-12-11 03:05:23 +0530

    learned about scanner types(for int,string)

--- frist-javaprogram/frist-idea-program/src/com/satyam/main.java
+++ frist-javaprogram/frist-idea-program/src/com/satyam/main.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println(input.nextInt());
+        // it inputs a int only//
+       // System.out.println(input.nextInt());
+        // it inputs a string//
+
+        //System.out.println(input.next()); // it will print till it finds spaces//
+        // to print entire //
+        System.out.println(input.nextLine());
+
+
     }
 }
